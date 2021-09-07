fix jaccard calculation and infer

train/data.py
@@ -3,7 +3,7 @@
 
 
 
-import configparser
+import collections
 import torch
 #import torchvision
 import numpy as np
@@ -131,6 +131,7 @@ def prepare_train_features(config, example, tokenizer):
     for i, offsets in enumerate(offset_mapping):
         feature = {}
         feature['context'] = example['context']
+        feature['answer_text'] = example["answer_start"]
 
         input_ids = tokenized_example["input_ids"][i]
         attention_mask = tokenized_example["attention_mask"][i]
@@ -138,6 +139,8 @@ def prepare_train_features(config, example, tokenizer):
         feature['input_ids'] = input_ids
         feature['attention_mask'] = attention_mask
         feature['offset_mapping'] = offsets
+        feature["example_id"] = example['id']
+        feature['sequence_ids'] = [0 if i is None else i for i in tokenized_example.sequence_ids(i)]
 
         cls_index = input_ids.index(tokenizer.cls_token_id)
         sequence_ids = tokenized_example.sequence_ids(i)
@@ -175,6 +178,34 @@ def prepare_train_features(config, example, tokenizer):
     return features
 
 
+def prepare_test_features(config, example, tokenizer):
+    example["question"] = example["question"].lstrip()
+
+    tokenized_example = tokenizer(
+        example["question"],
+        example["context"],
+        truncation="only_second",
+        max_length=config['MAX_LEN'],
+        stride=config['STRIDE'],
+        return_overflowing_tokens=True,
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
+
+    features = []
+    for i in range(len(tokenized_example["input_ids"])):
+        feature = {}
+        feature["example_id"] = example['id']
+        feature['context'] = example['context']
+        feature['question'] = example['question']
+        feature['input_ids'] = tokenized_example['input_ids'][i]
+        feature['attention_mask'] = tokenized_example['attention_mask'][i]
+        feature['offset_mapping'] = tokenized_example['offset_mapping'][i]
+        feature['sequence_ids'] = [0 if i is None else i for i in tokenized_example.sequence_ids(i)]
+        features.append(feature)
+    return features
+
+
 class DatasetRetriever(Dataset):
     def __init__(self, features, mode='train'):
         super(DatasetRetriever, self).__init__()
@@ -186,6 +217,7 @@ class DatasetRetriever(Dataset):
 
     def __getitem__(self, item):
         feature = self.features[item]
+        '''
         if self.mode == 'train':
             return {
                 'input_ids': torch.tensor(feature['input_ids'], dtype=torch.long),
@@ -195,16 +227,16 @@ class DatasetRetriever(Dataset):
                 'end_position': torch.tensor(feature['end_position'], dtype=torch.long),
                 'context': feature['context']
             }
-        else:
-            return {
-                'input_ids': torch.tensor(feature['input_ids'], dtype=torch.long),
-                'attention_mask': torch.tensor(feature['attention_mask'], dtype=torch.long),
-                'offset_mapping': feature['offset_mapping'],
-                'sequence_ids': feature['sequence_ids'],
-                'id': feature['example_id'],
-                'context': feature['context'],
-                'question': feature['question']
-            }
+        else:'''
+        return {
+            'input_ids': torch.tensor(feature['input_ids'], dtype=torch.long),
+            'attention_mask': torch.tensor(feature['attention_mask'], dtype=torch.long),
+            'offset_mapping': feature['offset_mapping'],
+            'sequence_ids': feature['sequence_ids'],
+            'id': feature['example_id'],
+            'context': feature['context'],
+            'question': feature['question']
+        }
 
 
 
@@ -361,8 +393,34 @@ def make_loader(
         drop_last=False
     )
     logging.info(f"loaders created, num steps: train-{len(train_dataloader)}, val-{len(valid_dataloader)}")
-    return train_dataloader, valid_dataloader
+    return train_dataloader, valid_dataloader, train_features, valid_features
 
+def make_test_loader(
+        config,
+        tokenizer,
+        df=None):
+
+    input_path = config['DATA_ROOT_PATH']
+
+    if df is None:
+        test = pd.read_csv(f'{input_path}/chaii-hindi-and-tamil-question-answering/test.csv')
+    else:
+        test = df
+
+    test_features = []
+    for i, row in test.iterrows():
+        test_features += prepare_test_features(config, row, tokenizer)
+
+    test_dataset = DatasetRetriever(test_features, mode='test')
+    test_dataloader = DataLoader(
+        test_dataset,
+        batch_size=config['VALID_BATCH_SIZE'],
+        sampler=SequentialSampler(test_dataset),
+        num_workers=optimal_num_of_loader_workers(),
+        pin_memory=True,
+        drop_last=False
+    )
+    return test_dataloader, test_features
 
 class CommonLitDataset(nn.Module):
     def __init__(self, data, tokenizer, config, reweighting=False):
@@ -446,8 +504,108 @@ class StratifiedBatchSampler:
     def __len__(self):
         return len(self.y)
 
+def postprocess_qa_predictions(tokenizer, features,
+                               all_start_logits, all_end_logits,
+                               n_best_size=20, max_answer_length=30):
+    features_per_example = collections.defaultdict(list)
+    for i, feature in enumerate(features):
+        features_per_example[feature["example_id"]].append(i)
+
+
+    predictions = collections.OrderedDict()
+
+    logging.info(f"Post-processing {len(features_per_example)} example predictions split into {len(features)} features.")
+
+    for example_id, features in features_per_example.items():
+
+        min_null_score = None
+        valid_answers = []
+
+        context = features[0]['context'] if len(features) > 0 else None
+        for feature_index in features:
+            start_logits = all_start_logits[feature_index]
+            end_logits = all_end_logits[feature_index]
+
+            sequence_ids = features[feature_index]["sequence_ids"]
+            context_index = 1
+
+            features[feature_index]["offset_mapping"] = [
+                (o if sequence_ids[k] == context_index else None)
+                for k, o in enumerate(features[feature_index]["offset_mapping"])
+            ]
+            offset_mapping = features[feature_index]["offset_mapping"]
+            cls_index = features[feature_index]["input_ids"].index(tokenizer.cls_token_id)
+            feature_null_score = start_logits[cls_index] + end_logits[cls_index]
+            if min_null_score is None or min_null_score < feature_null_score:
+                min_null_score = feature_null_score
+
+            start_indexes = np.argsort(start_logits)[-1: -n_best_size - 1: -1].tolist()
+            end_indexes = np.argsort(end_logits)[-1: -n_best_size - 1: -1].tolist()
+            for start_index in start_indexes:
+                for end_index in end_indexes:
+                    if (
+                            start_index >= len(offset_mapping)
+                            or end_index >= len(offset_mapping)
+                            or offset_mapping[start_index] is None
+                            or offset_mapping[end_index] is None
+                    ):
+                        continue
+                    # Don't consider answers with a length that is either < 0 or > max_answer_length.
+                    if end_index < start_index or end_index - start_index + 1 > max_answer_length:
+                        continue
+
+                    start_char = offset_mapping[start_index][0]
+                    end_char = offset_mapping[end_index][1]
+                    valid_answers.append(
+                        {
+                            "score": start_logits[start_index] + end_logits[end_index],
+                            "text": context[start_char: end_char]
+                        }
+                    )
+
+        if len(valid_answers) > 0:
+            best_answer = sorted(valid_answers, key=lambda x: x["score"], reverse=True)[0]
+        else:
+            best_answer = {"text": "", "score": 0.0}
+
+        predictions[example_id] = best_answer["text"]
+
+    return predictions
+
 
 # test...
 if __name__ == "__main__":
     from utils.config import TrainingConfig
-    print(get_data_kfold_split(TrainingConfig({'DATA_ROOT_PATH':'../../chaii/input/', 'USE_TRAIN_AS_TEST': False})))
+    config = TrainingConfig({'DATA_ROOT_PATH':'../../chaii/input/', 'USE_TRAIN_AS_TEST': False})
+    train, split_output = get_data_kfold_split(config)
+    print(train)
+    print(split_output)
+
+    from transformers import AutoTokenizer
+    tokenizer = AutoTokenizer.from_pretrained('deepset/xlm-roberta-large-squad2')
+    train_features = []
+    for i, row in train.iterrows():
+        exp = tokenizer(
+            row["question"],
+            row["context"],
+            truncation="only_second",
+            max_length=config['MAX_LEN'],
+            stride=config['STRIDE'],
+            return_overflowing_tokens=True,
+            return_offsets_mapping=True,
+            padding="max_length",
+        )
+        if len(exp['input_ids']) > 4:
+            print(f"question: {len(tokenizer(row['question'])['input_ids'])}")
+            print(f"context: {len(tokenizer(row['context'])['input_ids'])}")
+            #print(f"context: {row['context']}")
+            print(f"input_ids: {len(exp['input_ids'])}")
+            print(f"input_ids: {[len(x) for x in exp['input_ids']]}")
+
+        ret = prepare_train_features(config, row, tokenizer)
+        #f len(ret) > 1:
+        #    print(f"row: {row} -> features: {ret}")
+        train_features += ret
+
+    #nexp = pd.Series([ft['example_id'] for ft in train_features]).nunique()
+    print(f"features per example: {len(train_features)/len(train)}")

train/metric.py
@@ -5,6 +5,7 @@
 
 import configparser
 import torch
+import collections
 #import torchvision
 import numpy as np
 #import matplotlib.pyplot as plt
@@ -106,7 +107,7 @@ from transformers import (
 )
 import os
 from train.loss import loss_fn
-
+from train.data import postprocess_qa_predictions
 
 class AccumulateMeter(object):
     def __init__(self, previous_best=None):
@@ -115,8 +116,7 @@ class AccumulateMeter(object):
         self.last_best = None
 
     def reset(self, reset_best=False):
-        self.contexts = []
-        self.offset_mappings = []
+        self.features = []
 
         self.pred_starts = []
         self.pred_ends = []
@@ -127,23 +127,22 @@ class AccumulateMeter(object):
             self.best = None
             self.last_best = None
 
-    def update(self, contexts, offset_mappings, pred_starts, pred_ends, target_starts, target_ends):
+    def update(self, features, pred_starts, pred_ends, target_starts, target_ends):
         self.pred_starts.extend(pred_starts.tolist())
 
         self.pred_ends.extend(pred_ends.tolist())
         self.target_starts.extend(target_starts.tolist())
         self.target_ends.extend(target_ends.tolist())
-        self.contexts.extend(contexts)
-        self.offset_mappings.extend(offset_mappings)
+        self.features.extend(features)
 
-    def get_metrics(self):
+    def get_metrics(self, tokenzier):
         if len(self.contexts) == 0:
             return {}, False, self.last_best
 
         try:
-            res = get_metrics(self.contexts, self.offset_mappings, self.pred_starts, self.pred_ends, self.target_starts, self.target_ends)
+            res = get_metrics(self.features, tokenzier, self.pred_starts, self.pred_ends, self.target_starts, self.target_ends)
         except Exception as e:
-            logging.info(f"exception getting metric for: {(self.contexts, self.pred_starts, self.pred_ends, self.target_starts, self.target_ends)}")
+            logging.info(f"exception getting metric for: {(self.features, self.pred_starts, self.pred_ends, self.target_starts, self.target_ends)}")
             raise e
         is_best = False
         if self.best is None or res['jaccard'] > self.best:
@@ -160,7 +159,7 @@ def jaccard(str1, str2):
         return 0
     return float(len(c)) / (len(a) + len(b) - len(c))
 
-def get_metrics(contexts,offset_mappings, pred_starts, pred_ends, target_starts, target_ends):
+def get_metrics(features, tokenzier, pred_starts, pred_ends, target_starts, target_ends):
     metrics = ['loss', 'jaccard']
 
     res_dict = {}
@@ -172,10 +171,14 @@ def get_metrics(contexts,offset_mappings, pred_starts, pred_ends, target_starts,
             #for context, pred_start, pred_end, target_start, target_end in \
             #    zip(contexts, pred_starts, pred_ends, target_starts, target_ends):
             #    logging.info(f"{pred_start}:{pred_end}, {target_start}:{target_end}")
-            res = [jaccard(context[offset_mapping[np.argmax(pred_start)][0]:offset_mapping[np.argmax(pred_end)][1]+1],
-                           context[offset_mapping[target_start][0]:offset_mapping[target_end][1]+1])
-                   for context, offset_mapping, pred_start, pred_end, target_start, target_end in
-                   zip(contexts, offset_mappings, pred_starts, pred_ends, target_starts, target_ends)]
+            text_predictions = postprocess_qa_predictions(tokenzier, features, pred_starts, pred_ends)
+            example_id_to_answers = {}
+            for feat in features:
+                example_id_to_answers[feat['example_id']] = feat['answer_text']
+
+            res = [jaccard(pred_text,
+                           example_id_to_answers[example_id])
+                   for example_id, pred_text in text_predictions.items()]
             res = np.array(res).mean()
 
         res_dict[metric] = res

train/train.py
@@ -164,7 +164,79 @@ def save_run(original_model, paralelled_model, tokenizer, config, saving_dir, fo
 
     logging.info(f"saved in {saving_dir}/model_{fold}")
 
-def train_fn(data_loader, valid_loader, fold, model, optimizer, scheduler, device, config,
+
+def postprocess_qa_predictions(examples, features, raw_predictions, n_best_size=20, max_answer_length=30):
+    all_start_logits, all_end_logits = raw_predictions
+
+    example_id_to_index = {k: i for i, k in enumerate(examples["id"])}
+    features_per_example = collections.defaultdict(list)
+    for i, feature in enumerate(features):
+        features_per_example[example_id_to_index[feature["example_id"]]].append(i)
+
+    predictions = collections.OrderedDict()
+
+    print(f"Post-processing {len(examples)} example predictions split into {len(features)} features.")
+
+    for example_index, example in examples.iterrows():
+        feature_indices = features_per_example[example_index]
+
+        min_null_score = None
+        valid_answers = []
+
+        context = example["context"]
+        for feature_index in feature_indices:
+            start_logits = all_start_logits[feature_index]
+            end_logits = all_end_logits[feature_index]
+
+            sequence_ids = features[feature_index]["sequence_ids"]
+            context_index = 1
+
+            features[feature_index]["offset_mapping"] = [
+                (o if sequence_ids[k] == context_index else None)
+                for k, o in enumerate(features[feature_index]["offset_mapping"])
+            ]
+            offset_mapping = features[feature_index]["offset_mapping"]
+            cls_index = features[feature_index]["input_ids"].index(tokenizer.cls_token_id)
+            feature_null_score = start_logits[cls_index] + end_logits[cls_index]
+            if min_null_score is None or min_null_score < feature_null_score:
+                min_null_score = feature_null_score
+
+            start_indexes = np.argsort(start_logits)[-1: -n_best_size - 1: -1].tolist()
+            end_indexes = np.argsort(end_logits)[-1: -n_best_size - 1: -1].tolist()
+            for start_index in start_indexes:
+                for end_index in end_indexes:
+                    if (
+                            start_index >= len(offset_mapping)
+                            or end_index >= len(offset_mapping)
+                            or offset_mapping[start_index] is None
+                            or offset_mapping[end_index] is None
+                    ):
+                        continue
+                    # Don't consider answers with a length that is either < 0 or > max_answer_length.
+                    if end_index < start_index or end_index - start_index + 1 > max_answer_length:
+                        continue
+
+                    start_char = offset_mapping[start_index][0]
+                    end_char = offset_mapping[end_index][1]
+                    valid_answers.append(
+                        {
+                            "score": start_logits[start_index] + end_logits[end_index],
+                            "text": context[start_char: end_char]
+                        }
+                    )
+
+        if len(valid_answers) > 0:
+            best_answer = sorted(valid_answers, key=lambda x: x["score"], reverse=True)[0]
+        else:
+            best_answer = {"text": "", "score": 0.0}
+
+        predictions[example["id"]] = best_answer["text"]
+
+    return predictions
+
+def train_fn(data_loader, valid_loader,
+             train_features, valid_features,
+             fold, model, optimizer, scheduler, device, config,
              exp_record,
              original_model, tokenizer,
              saving_ts, saving_dir, epoch,best_loss_sum,previous_best
@@ -234,8 +306,8 @@ def train_fn(data_loader, valid_loader, fold, model, optimizer, scheduler, devic
         last_lr = scheduler.get_last_lr()
 
         # update meters
-        train_steps_meter.update(d['context'], d['offset_mapping'], outputs_start, outputs_end, targets_start, targets_end)
-        train_total_meter.update(d['context'], d['offset_mapping'], outputs_start, outputs_end, targets_start, targets_end)
+        train_steps_meter.update(d, outputs_start, outputs_end, targets_start, targets_end)
+        train_total_meter.update(d, outputs_start, outputs_end, targets_start, targets_end)
 
         if step >= (last_train_eval_step + train_eval_period) or idx == (len(data_loader) -1):
             # Evaluate the model on train_loader.
@@ -311,7 +383,7 @@ def eval(data_loader, model, device, config, previous_best):
             
             outputs_start, outputs_end = outputs
 
-            meter.update(d['context'], d['offset_mapping'], outputs_start, outputs_end, targets_start, targets_end)
+            meter.update(d, outputs_start, outputs_end, targets_start, targets_end)
 
         #sp_cor = 0 #spearmanr(fin_targets, fin_outputs)
         
@@ -327,50 +399,47 @@ def eval(data_loader, model, device, config, previous_best):
 # for debug...
 printed_debug_info = True
 
-def infer(data_loader, model, device, config, return_embed = False, use_tqdm=True):
+def infer(data_loader, model, device, config, tokenizer, use_tqdm=True):
     global printed_debug_info
     printed_debug_info = True
     model.eval()
+    to_ret = {}
     with torch.no_grad():
-        fin_targets = []
-        fin_outputs = []
-        if return_embed:
-            fin_outputs_embed = []
+        outputs_starts = []
+        outputs_ends = []
+
         to_for = enumerate(data_loader)
         if use_tqdm:
             to_for = tqdm(to_for, total = len(data_loader))
-        for idx, data in to_for:
-            data = {key:val.reshape(val.shape[0],-1).to(device) for key,val in data.items()}
-            
+        for idx, d in to_for:
+
+
+            model_input_keys = ['input_ids', 'attention_mask']
+            data = {key: val.reshape(val.shape[0], -1).to(device) for key, val in d.items() if key in model_input_keys}
+
+
             
             if not printed_debug_info:
                 logging.info(f"input to model:{data}")
                 logging.info(f"input to model shape:{data['attention_mask'].shape}")
                 
-            outputs, embeds = model(**data)
+            outputs_start, outputs_end = model(**data)
             if not printed_debug_info:
-                logging.info(f"output to model:{outputs}")
-                logging.info(f"output to model shape:{outputs.shape}")
+                logging.info(f"output to model:{outputs_start}")
+                logging.info(f"output to model shape:{outputs_start.shape}")
+                logging.info(f"output to model:{outputs_end}")
+                logging.info(f"output to model shape:{outputs_end.shape}")
                 
                 printed_debug_info = True
-            outputs = outputs.squeeze(-1)
-            
-            #outputs = outputs["logits"].squeeze(-1)
-            
-            if config['LOSS_TYPE'] == 'multi-class':
-                outputs = torch.argmax(outputs, dim=-1)
+            outputs_start = outputs_start.squeeze(-1)
+            outputs_end = outputs_end.squeeze(-1)
 
-#             targets = data['targets']
-
-#             outputs = model(data['input_ids'], data['attention_mask'])
             
-            fin_outputs.extend(outputs.detach().cpu().numpy().tolist())
-            if return_embed:
-                fin_outputs_embed.extend(embeds.detach().cpu().numpy())
-    if return_embed:
-        return fin_outputs, fin_outputs_embed 
-    else:
-        return fin_outputs
+            outputs_starts.extend(outputs_start.detach().cpu().tolist())
+            outputs_ends.extend(outputs_end.detach().cpu().tolist())
+
+
+    return outputs_starts, outputs_ends
 
 
 # In[ ]:
@@ -436,7 +505,7 @@ def run(config, import_file_path=None):
 
     for fold, (train_idx,valid_idx) in enumerate(tqdm(split_output, total=len(split_output))):
 
-        train_loader, valid_loader = make_loader(config, data, split_output, tokenizer, fold)
+        train_loader, valid_loader, train_features, valid_features = make_loader(config, data, split_output, tokenizer, fold)
             
         if config['RESEED_EVERY_FOLD']:
             seed_everything(config['SEED'] + fold)
@@ -491,15 +560,18 @@ def run(config, import_file_path=None):
 
 
             logging.info(f'========== epoch : {epoch+1}==========')
-            rec, best_metric = train_fn(train_loader, valid_loader,fold,
-                model,
-                optimizer,
-                scheduler,
-                device,
-                config,
-                exp_record,
-                original_model, tokenizer,
-                saving_ts, saving_dir, epoch, best_loss_sum, previous_best=best_metric)
+            rec, best_metric = train_fn(train_loader, valid_loader,
+                                        train_features, valid_features,
+                                        fold,
+                                        model,
+                                        optimizer,
+                                        scheduler,
+                                        device,
+                                        config,
+                                        exp_record,
+                                        original_model, tokenizer,
+                                        saving_ts, saving_dir, epoch, best_loss_sum,
+                                        previous_best=best_metric)
         
         end_time = time.time()
         elp_fold = end_time - start_time
@@ -523,32 +595,24 @@ def run(config, import_file_path=None):
 
     
 def pred_df(df, config, upload_name='pretrained-model-1621892031'):
-    pretrain_base_path = f'crp/data/{upload_name}/' if 'gavin_li' in cwd else f'../input/{upload_name}/'
+    root_path = config['PRED_ROOT_PATH']
+    pretrain_base_path = f'{root_path}/{upload_name}/'
     
     if 'SEED' in config:
         seed_everything(config['SEED'])
     else:
         seed_everything(43)
     
-    #print(pretrain_base_path)
 
     import json
-    
-    # assert the save version...
-    
-    #with open(f"{pretrain_base_path}/model_0/training_config.json", 'r') as f:
-    #    conf = json.load(f)
-    #    if 'saving_ts' in conf:
-    #        assert conf['saving_ts'] == 1622060846, 'saving_ts should match with the saved one'
-        
-        
+
+
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 
     import os
 
     from pathlib import Path
-    from transformers.file_utils import WEIGHTS_NAME
     pretrain_paths = []
 
     pathlist = Path(pretrain_base_path).glob('**/*')
@@ -566,97 +630,35 @@ def pred_df(df, config, upload_name='pretrained-model-1621892031'):
 
     # fix GPT2 save pretrain issue...
     logging.info(f"loading tokenizer for {config['TOKENIZER']}")
-    if config['TOKENIZER'] == 'microsoft/deberta-base':
-        logging.info(f"fix GPT2 save pretrain issue by direct load from huggingface registry...")
-        if os.path.exists("../input/deberta-base-uncased-tokenizer"):
-            logging.info(f"loading from ../input/deberta-base-uncased-tokenizer")
-            tokenizer = DebertaTokenizer.from_pretrained(f"../input/deberta-base-uncased-tokenizer")
-        elif os.path.exists(f"{str(pretrain_paths[0])}/tokenizer/"):
-            logging.info(f"loading from {pretrain_paths[0]}/tokenizer/")
-            tokenizer = DebertaTokenizer.from_pretrained(f"{str(pretrain_paths[0])}/tokenizer/")
-        else:
-            logging.info(f"loading from {pretrain_paths[0]}")
-            tokenizer = DebertaTokenizer.from_pretrained(f"{str(pretrain_paths[0])}")
-    else:
-        if os.path.exists(f"{str(pretrain_paths[0])}/tokenizer/"):
-            if config['TOKENIZER'] in ["deepset/roberta-base-squad2","chkla/roberta-argument","roberta-base", "deepset/roberta-large-squad2"]:
-                tmp_ts = int(time.time())
-                os.system(f'mkdir tmp_{tmp_ts}')
-                tokenizer_dir = f"{str(pretrain_paths[0])}/tokenizer/"
-                from_dir = f"{str(pretrain_paths[0])}/"
-                os.system(f'cp {tokenizer_dir}/* tmp_{tmp_ts}/')
-                os.system(f'cp {from_dir}/config.json tmp_{tmp_ts}/')
-                tokenizer = AutoTokenizer.from_pretrained(f"tmp_{tmp_ts}/")
-            else:
-                logging.info(f"loading from {pretrain_paths[0]}/tokenizer/")
-                tokenizer = AutoTokenizer.from_pretrained(f"{str(pretrain_paths[0])}/tokenizer/")
-        else:
-            logging.info(f"loading from {pretrain_paths[0]}")
-            tokenizer = AutoTokenizer.from_pretrained(f"{str(pretrain_paths[0])}")
+    logging.info(f"loading from {pretrain_paths[0]}/tokenizer/")
+    tokenizer = AutoTokenizer.from_pretrained(f"{str(pretrain_paths[0])}/tokenizer/")
 
-    sub_ds = CommonLitDataset(df, tokenizer, config)    
-    
-    sub_ds_loader = torch.utils.data.DataLoader(
-            sub_ds,
-            batch_size = config['VALID_BATCH_SIZE'],
-            num_workers = 2,
-            drop_last=False,
-        )
+    sub_ds_loader,features = make_test_loader(config, tokenizer, df=df)
     
     
-    scores = []
-    embeds = []
+    start_logits = []
+    end_logits = []
 
     for p in pretrain_paths:
-
-        #model = AutoModelForSequenceClassification.from_pretrained(p,num_labels=1)
-        model_class = getattr(model, config['MODEL_CLASS'])
+        model_class = getattr(model_import, config['MODEL_CLASS'])
         
         # hardcode upload fix:
-        if "single-model-v7-1627797474/model_3" in p:
-            model_config = json.load(open(f'../input/single-model-v7-1627797474/model_2/training_config.json'))
-            subprocess.getoutput("mkdir /tmp/model_3")
-            subprocess.getoutput("cp ../input/single-model-v7-1627797474/model_3/*.bin /tmp/model_3/")
-            subprocess.getoutput("cp ../input/single-model-v7-1627797474/model_2/*.json /tmp/model_3/")
-            subprocess.getoutput("cp -r ../input/single-model-v7-1627797474/model_2/tokenizer /tmp/model_3/")
-        else:
-            
-            model_config = json.load(open(f'{p}/training_config.json'))
-            
-        model_config = {**base_config, **model_config}
-        
+        model_config = TrainingConfig(json.load(open(f'{p}/training_config.json')))
+
         logging.info(f"loading model class:{config['MODEL_CLASS']}\n pretrain: {str(p)}, config:{model_config}")
       
         if model_config['EMBED_OTHER_GPU'] is not None:
             model_config['EMBED_OTHER_GPU'] = 0
 
             
-        if "single-model-v7-1627797474/model_3" in p:
-            model = model_class(from_pretrain='/tmp/model_3', model_config=model_config)
-            model.load_checkpoint('/tmp/model_3')
-        else:
-            model = model_class(from_pretrain=p, model_config=model_config)
-            model.load_checkpoint(p)
+        model = model_class(from_pretrain=p, model_config=model_config)
+        model.load_checkpoint(p)
         model.to(device)
-        
-        #print('loading checkpoint...')
-    
-        #checkpoint = torch.load(os.path.join(p, WEIGHTS_NAME), map_location=torch.device('cpu'))
-        #model.load_state_dict(checkpoint,strict=False)
 
-        #print(f'{os.path.join(p, WEIGHTS_NAME)} loaded.')
+        pred_start, pred_end = infer(sub_ds_loader,model,device, model_config, tokenizer, return_embed=True)
 
-        
-        #print(f"input to infer:{df}")
-        outputs, output_embeds = infer(sub_ds_loader,model,device, model_config, return_embed=True)
-        #print(f"output to infer:{output_embeds}")
-
-        #print(sum)
-        logging.info(outputs)
-
-        pred_sum += outputs
-        scores.append(outputs)
-        embeds.append(output_embeds)
+        start_logits += pred_start
+        end_logits += pred_end
         
         
         # cleanup after fold is done
@@ -666,8 +668,15 @@ def pred_df(df, config, upload_name='pretrained-model-1621892031'):
         torch.cuda.empty_cache()
         
 
-    pred_sum = pred_sum/(len(pretrain_paths))
-    return pred_sum, scores, embeds
+    start_logits = start_logits/(len(pretrain_paths))
+    end_logits = end_logits/(len(pretrain_paths))
+
+    preds = postprocess_qa_predictions(tokenizer, features,
+                                       start_logits, end_logits)
+
+    df['PredictionString'] = df['id'].map(preds)
+
+    return df
 
 
 # In[ ]:
@@ -687,40 +696,36 @@ def create_submission(_,predictions, calibrate_rms=None):
 # In[ ]:
 
 
-def gen_submission(TRAIN_MODE=False, TEST_ON_TRAINING=True, gen_file=True):
+def gen_submission(train, test, TRAIN_MODE=False, TEST_ON_TRAINING=True, gen_file=True):
     to_ret = None
     if not TRAIN_MODE:
         if TEST_ON_TRAINING:
             # test first...
-            pred_sum_train, _, _ = pred_df(train[['excerpt','id']], config)
+            res_df = pred_df(train[['excerpt','id']], config)
+            res_df['jaccard'] = res_df.apply(lambda x: jaccard(x['answer_text'], x['PredictionString']), axis=1)
 
+            jaccard_metric = res_df['jaccard'].mean()
+            assert jaccard_metric > 0.85
 
-            loss_on_train = loss_fn(torch.tensor(pred_sum_train), torch.tensor(train['target'].values), config, loss_type='sqrt_mse').item()
-            assert loss_on_train < 0.55
+            logging.info(f"loss on training: {jaccard_metric}")
 
-            logging.info(f"loss on training: {loss_on_train}")
+        res_df = pred_df(test, config)
 
-        pred_sum, _, _ = pred_df(test, config)
-        
-        to_ret = pred_sum
-
-
-        pred = create_submission(test, pred_sum)
+        pred = res_df[['id', 'PredictionString']]
         logging.info(pred.head())
     else:
         # test infer on training set when it's training mode...
-        pred_sum_train, _, _ = pred_df(train[['excerpt','id']], config)
-        loss_on_train = loss_fn(torch.tensor(pred_sum_train), torch.tensor(train['target'].values), config, loss_type='sqrt_mse').item()
-        assert loss_on_train < 0.55, f"{loss_on_train} shoudl be small"
+        res_df = pred_df(train[['excerpt','id']], config)
+        res_df['jaccard'] = res_df.apply(lambda x: jaccard(x['answer_text'], x['PredictionString']), axis=1)
 
-        logging.info(f"loss on training: {loss_on_train}")
-        to_ret = pred_sum_train
+        jaccard_metric = res_df['jaccard'].mean()
+        assert jaccard_metric > 0.85
+
+        logging.info(f"loss on training: {jaccard_metric}")
 
         
     if not TRAIN_MODE and gen_file:
         pred.to_csv('./submission.csv',index=False)
-        
-    return to_ret
 
 
 # In[ ]: