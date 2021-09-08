0c51c1fef04a9e44674188f1f802cf676e19cb1a
lyogavin <lyo.gavin@gmail.com>
2021-09-08 22:43:21 +0800

    fix data loading in infer

--- run_infer.py
+++ run_infer.py
@@ -1,12 +1,17 @@
 
 from train import train
 from utils.config import TrainingConfig
-from train.infer import infer_and_gen_submission
+from train.infer import infer_and_gen_submission, download_saving
 import sys
 
 if __name__ == "__main__":
 
-    assert len(sys.argv) > 1, 'need to specify saving ts'
+    assert len(sys.argv) > 1, 'need to specify saving ts to infer or ts and download url to download'
 
     saving_ts=sys.argv[1]
-    infer_and_gen_submission(saving_ts, TRAIN_MODE=False, TEST_ON_TRAINING=True, gen_file=True)
+
+    if len(sys.argv) > 2:
+        url = sys.argv[2]
+        download_saving(url, saving_ts)
+    else:
+        infer_and_gen_submission(saving_ts, TRAIN_MODE=False, TEST_ON_TRAINING=True, gen_file=True)
