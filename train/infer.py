62759f347534391beb78a19c81a71e835b0cf9f4
lyogavin <lyo.gavin@gmail.com>
2021-10-03 21:12:13 +0800

    fix logits save

--- train/infer.py
+++ train/infer.py
@@ -292,6 +292,9 @@ def pred_df(df, pretrain_base_path, nbest=False, return_logits=False):
     start_logits = start_logits/(len(pretrain_paths))
     end_logits = end_logits/(len(pretrain_paths))
 
+    ret_start_logits = start_logits.tolist().copy()
+    ret_end_logits = end_logits.tolist().copy()
+
     if not nbest:
         preds = postprocess_qa_predictions(tokenizer, features,
                                            start_logits.tolist(),
@@ -314,7 +317,7 @@ def pred_df(df, pretrain_base_path, nbest=False, return_logits=False):
     if not return_logits:
         return ret_df
     else:
-        return ret_df, start_logits.tolist(), end_logits.tolist(), features
+        return ret_df, ret_start_logits, ret_end_logits, features
 # In[ ]:
 
 from sklearn.metrics import mean_squared_error
