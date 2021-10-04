16442c6705ba548277bf535600137dc15033534c
lyogavin <lyo.gavin@gmail.com>
2021-10-04 13:14:14 +0800

    fix logits save

--- train/infer.py
+++ train/infer.py
@@ -298,7 +298,6 @@ def pred_df(df, pretrain_base_path, nbest=False, return_logits=False):
     ret_start_logits = start_logits.tolist().copy()
     ret_end_logits = end_logits.tolist().copy()
 
-    logging.info(f"infer output: {np.array(ret_start_logits).shape}")
     if not nbest:
         preds = postprocess_qa_predictions(tokenizer, features,
                                            start_logits.tolist(),
@@ -321,7 +320,6 @@ def pred_df(df, pretrain_base_path, nbest=False, return_logits=False):
     if not return_logits:
         return ret_df
     else:
-        logging.info(f"infer output: {np.array(ret_start_logits).shape}")
         return ret_df, ret_start_logits, ret_end_logits, features
 # In[ ]:
 
@@ -421,6 +419,7 @@ def infer_and_save_inter_outputs(saving_ts, input_base_path, output_base_path, u
                                                return_logits=output_logits)
 
     if test_mode:
+        logging.info(f"logits shape: {np.array(start_logits).shape}")
         logging.info(f"logits count: {len(start_logits)}")
         logging.info(f"logits: {[len(x[0]) if isinstance(x[0], list) else 0 for x in start_logits]}, "
                      f"{[len(x[1]) if isinstance(x[0], list) else 0  for x in start_logits]}")
