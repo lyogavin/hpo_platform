1eebef33e13bd7fdf400ac75980dd5ab9d1d1c38
lyogavin <lyo.gavin@gmail.com>
2021-10-18 01:22:22 +0800

    fix length exceeding

--- train/data.py
+++ train/data.py
@@ -654,8 +654,8 @@ def postprocess_qa_predictions(tokenizer, features,
                 #start_logits=np.where(sequence_ids != 0, start_logits, np.min(start_logits))
                 #end_logits=np.where(sequence_ids != 0, end_logits, np.min(end_logits))
 
-                start_logits[len(features[feature_index]["context"]):] = np.min(start_logits)
-                end_logits[len(features[feature_index]["context"]):] = np.min(end_logits)
+                start_logits[len(features[feature_index]["context"]):] = [np.min(start_logits) for x in range(len(start_logits) - len(features[feature_index]["context"]))]
+                end_logits[len(features[feature_index]["context"]):] = [np.min(end_logits) for x in range(len(end_logits) - len(features[feature_index]["context"]))]
 
                 start_char = np.argmax(start_logits)
                 end_char = np.argmax(end_logits)
