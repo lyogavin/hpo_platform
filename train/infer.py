0911426881a9ef7ff89bf0999374bb7423ff673f
lyogavin <lyo.gavin@gmail.com>
2021-10-18 19:50:52 +0800

    test split

--- train/infer.py
+++ train/infer.py
@@ -541,6 +541,7 @@ def char_model_infer_and_gen_submission(saving_ts,
 
     # test split...
     if test_split_config is not None:
+        logging.info('testing split...')
         data, split_output = get_data_kfold_split(config)
         assert len(train) == len(data)
 
